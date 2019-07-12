efce320c420efbd07389d42f8aee3efc510f0dff
Anglebert Masunda <37086822+Eng-Masunda@users.noreply.github.com>
2019-07-12 14:02:30 +0200

    Added files to repository

---
+++ js/app.js
@@ -0,0 +1,10 @@
+//Hamburger menu
+const menu = document.querySelector(".menu");
+
+const hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener('click', () => {
+    menu.classList.toggle("active");
+})
+
+
